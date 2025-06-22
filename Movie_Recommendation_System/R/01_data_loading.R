bc541fb6e4a766c0d9999d76f95374d7e98bc2e4
Naman Dhanopiya <iamnamandhanopiya@gmail.com>
2025-06-22 14:31:53 +0530

    Added comments

--- Movie_Recommendation_System/R/01_data_loading.R
+++ Movie_Recommendation_System/R/01_data_loading.R
@@ -1,11 +1,11 @@
-# Set custom library path
+# Set the path for R packages to be loaded
 .libPaths("D:/Movie_Recommendation_System/Rlibs")
 
 # Load required packages
 library(dplyr)
 library(tibble)
 
-# Function to load data
+# Function to load the movie recommendation dataset
 load_data <- function(path = "data/ml-100k/") {
   ratings <- read.table(file.path(path, "u.data"), sep = "\t",
                         col.names = c("user_id", "item_id", "rating", "timestamp"))
@@ -15,9 +15,10 @@ load_data <- function(path = "data/ml-100k/") {
 
   users <- read.table(file.path(path, "u.user"), sep = "|",
                       col.names = c("user_id", "age", "gender", "occupation", "zip_code"))
-
-  # Rename movie columns
+  
+  # Rename first two columns of movie data
   colnames(movies)[1:2] <- c("item_id", "title")
 
+  # Return all three datasets in a list
   return(list(ratings = ratings, movies = movies, users = users))
 }
