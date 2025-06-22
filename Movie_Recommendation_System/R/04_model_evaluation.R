f0f7bc0b50c3734850285f8b129dc785cd620338
Naman Dhanopiya <iamnamandhanopiya@gmail.com>
2025-06-22 14:36:58 +0530

    Updated documentation

--- Movie_Recommendation_System/R/04_model_evaluation.R
+++ Movie_Recommendation_System/R/04_model_evaluation.R
@@ -1,12 +1,16 @@
+# Set the path for R packages to be loaded
 .libPaths("D:/Movie_Recommendation_System/Rlibs")
 
+# Function that evaluates the recommendation model
 evaluate_model <- function(model, rating_matrix) {
+  # Loading recommenderlab package {includes tools for model evaluation}
   library(recommenderlab)
-  
+
+  # Split data into 5 parts and use 10 ratings per user to train the model
   scheme <- evaluationScheme(rating_matrix, method="cross-validation", k=5, given=10)
   results <- evaluate(scheme, method="UBCF", type="ratings")
   
-  rmse <- avg(results)["RMSE"]
-  cat("Average RMSE:", rmse, "\n")
+  rmse <- avg(results)["RMSE"]                                # Calculates the average RMSE (Root Mean Square Error)
+  cat("Average RMSE:", rmse, "\n")                            # Prints the RMSE value
   return(rmse)
 }
