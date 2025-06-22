29b488a59379f9d3e2f09b113baf43538f38ea7f
Naman Dhanopiya <iamnamandhanopiya@gmail.com>
2025-06-22 14:35:34 +0530

    Updated documentation

--- Movie_Recommendation_System/R/03_model_building.R
+++ Movie_Recommendation_System/R/03_model_building.R
@@ -1,9 +1,16 @@
+# Set the path for R packages to be loaded
 .libPaths("D:/Movie_Recommendation_System/Rlibs")
 
+# Function to build the recommendation model
 build_model <- function(ratings) {
+  # Loads the recommenderlab package
   library(recommenderlab)
-  
+
+  # Converts ratings data into a special matrix format required by recommenderlab
   rating_matrix <- as(ratings[, c("user_id", "item_id", "rating")], "realRatingMatrix")
-  model <- Recommender(rating_matrix, method = "UBCF")  # User-Based Collaborative Filtering
+  # Trains a recommendation model using User-Based Collaborative Filtering
+  model <- Recommender(rating_matrix, method = "UBCF")
+
+  # Returns the trained model and the rating matrix
   return(list(model = model, matrix = rating_matrix))
 }
