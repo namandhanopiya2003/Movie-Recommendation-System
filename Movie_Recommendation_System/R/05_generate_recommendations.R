f58fe71a6754b10f802bf6c73c22290740e816ae
Naman Dhanopiya <iamnamandhanopiya@gmail.com>
2025-09-09 18:51:20 +0530

    Updated documentation

--- Movie_Recommendation_System/R/05_generate_recommendations.R
+++ Movie_Recommendation_System/R/05_generate_recommendations.R
@@ -1,23 +1,34 @@
+# Sets the path to store/load R packages
 .libPaths("D:/Movie_Recommendation_System/Rlibs")
 
+# Load dplyr for data manipulation
 library(dplyr)
 
+# This function creates movie recommendations for each user using the trained model
 generate_recommendations <- function(model, rating_matrix, movies_df, n = 5) {
+  # It gets top n recommended movies for each user from the model
   top_n <- predict(model, rating_matrix, n = n)
+  # Converts recommendations into a list where each user has their movie IDs
   recommendations <- as(top_n, "list")
-  
+
+  # Empty list to store recommendation details
   rec_list <- list()
-  
+
+  # Loop through each user to build their recommendations
   for(user_id in names(recommendations)) {
+    # Movies recommended to this particular user
     rec_movies <- recommendations[[user_id]]
-    
+
+    # For each recommended movie, get its title and assign a randomly grnerated confidence score
     for(movie_id in rec_movies) {
       movie_title <- movies_df %>%
         filter(item_id == as.integer(movie_id)) %>%
         pull(title)
-      
+
+      # This confidence score is added only to make the output look more appealing and does not represent any real calculated confidence.
       confidence <- round(runif(1, 80, 99), 1)  
-      
+
+      # Adds this recommendation info to the list
       rec_list <- append(rec_list, list(data.frame(
         user_id = user_id,
         movie_id = movie_id,
@@ -26,10 +37,13 @@ generate_recommendations <- function(model, rating_matrix, movies_df, n = 5) {
       )))
     }
   }
-  
+
+  # Combines all individual recommendations into one big table
   rec_df <- do.call(rbind, rec_list)
-  
+
+  # Saves the recommendations as a CSV file for later use
   write.csv(rec_df, "output/recommendations.csv", row.names = FALSE)
 
   return(rec_df)
 }
+
